7487c3b77d33693d2a703519d9aad2e260a649ea
raulms10 <rantonio.martinez@udea.edu.co>
2017-06-07 15:02:32 -0500

    Lista de dispositivos

--- ServiciosWeb/WebContent/resources/js/usuario.js
+++ ServiciosWeb/WebContent/resources/js/usuario.js
@@ -1,9 +1,9 @@
 var appCliente = angular.module('usuario', ['ngRoute', 'ngCookies']);
 
-appCliente.service('usuario', function ($http, $cookies, $location) {
+appCliente.service('users', function ($http, $cookies, $location) {
     this.autenticar = function (username, password) {
         return $http({
-            url: 'http://localhost:8080/PrestamosDispositivos/api/usuario',
+            url: 'http://localhost:8080/ServiciosWeb/prestamos/user/login',
             method: 'GET',
             params: {
                 username: username,
@@ -15,7 +15,7 @@ appCliente.service('usuario', function ($http, $cookies, $location) {
 
     this.registrar = function (username, typeId, numberId, name, lastName, email, password, role, manager) {
         return $http({
-            url: 'http://localhost:8080/PrestamosDispositivos/api/usuario',
+            url: 'http://localhost:8080/ServiciosWeb/prestamos/user/register',
             method: 'POST',
             params: {
                 username: username,
@@ -30,17 +30,21 @@ appCliente.service('usuario', function ($http, $cookies, $location) {
             }
         });
     };
+});
 
+appCliente.service('devices', function ($http, $cookies, $location) {
     this.getDispositivos = function () {
         return $http({
-            url: 'http://localhost:8080/PrestamosDispositivos/api/dispositivo',
+            url: 'http://localhost:8080/ServiciosWeb/prestamos/device/all',
             method: 'GET'
         });
     };
-    
+});
+
+appCliente.service('loans', function ($http, $cookies, $location) {
     this.getPrestamosDispositivo = function (code,copy,date) {
         return $http({
-            url: 'http://localhost:8080/PrestamosDispositivos/api/prestamo/dispositivo',
+            url: 'http://localhost:8080/ServiciosWeb/prestamos/loan/get',
             method: 'GET',
             params:{
                 code:code,
@@ -52,10 +56,60 @@ appCliente.service('usuario', function ($http, $cookies, $location) {
 });
 
 appCliente.controller('pruebaController', function ($scope,$location) {
-	$location.url("/login")
+	//$location.url("/login")
 });
 
-appCliente.controller('usuarioController', function ($scope, $cookies, $window, $log, usuarios) {
+appCliente.controller('dispositivoController', function ($scope,$location,devices) {
+	//$location.url("/login")
+	//$scope.getDispositivos = function () {
+        //$scope.dispositivos = [];
+        devices.getDispositivos().then(
+                function success(data) {
+
+                    if (data.data != '') {
+                    	$scope.listaDevices = data.data.device;
+                    	
+                    	/*angular.forEach(data.data,function(dat){
+                            $scope.dispositivos.push(dat);
+                        });*/
+                    }
+                },
+                function failure(data) {
+                    alert("falla " + data.data);
+                }
+        );
+    //};
+    
+    
+    
+});
+
+appCliente.controller('prestamoController', function ($scope,$location) {
+	//$location.url("/login")
+	$scope.reservar = function (dispositivo) {
+        $scope.prestamos = [];
+        $scope.date = new Date();
+        alert(dispositivo.deviceId.code);
+        usuarios.getPrestamosDispositivo(dispositivo.deviceId.code,dispositivo.deviceId.copy,$scope.date).then(
+                function success(data) {
+
+                    if (data.data != '') {
+                        angular.forEach(data.data,function(dat){
+                            $scope.prestamos.push(dat);
+                            alert(dat.status);
+                        });
+                    }
+                    $location.url("/")
+                    //$window.location.href = "http://localhost:8080/PrestamosDispositivos/reserva.html";
+                },
+                function failure(data) {
+                    alert("falla " + data.data);
+                }
+        );
+    };
+});
+
+appCliente.controller('usuarioController', function ($scope, $cookies, $location, $log, users) {
 
     $scope.username = '';
     $scope.typeId = '';
@@ -74,15 +128,17 @@ appCliente.controller('usuarioController', function ($scope, $cookies, $window, 
                 function success(data) {
                     if (data.data != '') {
                         $scope.password = '';
-                        if (data.data == "S") {
+                        if (data.data.type != "error") {
                             
                             $scope.nombre = $scope.username;
                             $cookies.username = $scope.username;
                             console.log($scope.nombre);
                             alert($scope.nombre);
-                            $window.location.href = "http://localhost:8080/PrestamosDispositivos/usuario.html";
+                            $location.url("/")
+                            //$window.location.href = "http://localhost:8080/PrestamosDispositivos/usuario.html";
                         } else {
-                            alert("Usuario incorrecto");
+                            alert(data.data.message);
+                            $location.url("/")
                         }
                     }
 
@@ -108,48 +164,11 @@ appCliente.controller('usuarioController', function ($scope, $cookies, $window, 
                     $scope.password = '';
                     $scope.role = '';
                     $scope.manager = '';
-                    $window.location.href = "http://localhost:8080/PrestamosDispositivos/login.html";
+                    $location.url("/")
+                    //$window.location.href = "http://localhost:8080/PrestamosDispositivos/login.html";
                 },
                 function failure(data) {
                     alert("Error");
-                }
-        );
-    };
-    
-    $scope.getDispositivos = function () {
-        $scope.dispositivos = [];
-        usuarios.getDispositivos().then(
-                function success(data) {
-
-                    if (data.data != '') {
-                        angular.forEach(data.data,function(dat){
-                            $scope.dispositivos.push(dat);
-                        });
-                    }
-                },
-                function failure(data) {
-                    alert("falla " + data.data);
-                }
-        );
-    };
-    
-    $scope.reservar = function (dispositivo) {
-        $scope.prestamos = [];
-        $scope.date = new Date();
-        alert(dispositivo.deviceId.code);
-        usuarios.getPrestamosDispositivo(dispositivo.deviceId.code,dispositivo.deviceId.copy,$scope.date).then(
-                function success(data) {
-
-                    if (data.data != '') {
-                        angular.forEach(data.data,function(dat){
-                            $scope.prestamos.push(dat);
-                            alert(dat.status);
-                        });
-                    }
-                    $window.location.href = "http://localhost:8080/PrestamosDispositivos/reserva.html";
-                },
-                function failure(data) {
-                    alert("falla " + data.data);
                 }
         );
     };
@@ -159,7 +178,7 @@ appCliente.controller('usuarioController', function ($scope, $cookies, $window, 
 appCliente.config(['$routeProvider',function($routeProvider){
 
 	$routeProvider.when('/',{
-		templateUrl: 'index.html',
+		templateUrl: 'main.html',
 		controller:'pruebaController'
 	});
 	
@@ -168,5 +187,16 @@ appCliente.config(['$routeProvider',function($routeProvider){
 		controller:'usuarioController'
 	});
 	
+	$routeProvider.when('/devices',{
+		templateUrl: 'devices.html',
+		controller:'dispositivoController'
+	});
+	
 }]);
+
+/*appCliente.run(function($rootScope, users){
+	$rootScope.$on('$routeChangeStart', function(){
+		users.validarEstado();
+	})
+});*/
 
